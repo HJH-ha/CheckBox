0b72e8fd0ec4b8f2be24b937ffa1e66ba683ba1a
JH <wlgud5632@naver.com>
2024-10-31 11:38:28 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// alert("test");
+
+function isChecked() {
+  const checkBox = document.getElementById("my-checkbox");
+  const message = document.getElementById("message");
+
+  if (checkBox.checked) {
+    message.textContent = "체크됨!";
+  } else {
+    message.textContent = "체크안됨";
+  }
+}
